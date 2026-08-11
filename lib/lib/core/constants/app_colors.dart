45bd5def19eea9bc9f8d671b83782a1991d4cd2d
fofa19991-creator <fofa19991@gmail.com>
2026-08-11 17:30:46 +0300

    Create app_colors.dart

---
+++ lib/lib/core/constants/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Palette
+  static const Color royalBlue = Color(0xFF0F4C81);
+  static const Color royalBlueDark = Color(0xFF0A3357);
+  static const Color lightGold = Color(0xFFD4AF37);
+  static const Color accentGold = Color(0xFFF3E5AB);
+  
+  // Neutral Colors
+  static const Color backgroundLight = Color(0xFFF8F9FA);
+  static const Color surfaceLight = Color(0xFFFFFFFF);
+  static const Color textPrimaryLight = Color(0xFF1E293B);
+  static const Color textSecondaryLight = Color(0xFF64748B);
+
+  // Dark Theme Neutral Colors
+  static const Color backgroundDark = Color(0xFF0F172A);
+  static const Color surfaceDark = Color(0xFF1E293B);
+  static const Color textPrimaryDark = Color(0xFFF8FAFC);
+  static const Color textSecondaryDark = Color(0xFF94A3B8);
+
+  // Status & Card Borders
+  static const Color cardBorder = Color(0xFFE2E8F0);
+  static const Color cardBorderDark = Color(0xFF334155);
+}
